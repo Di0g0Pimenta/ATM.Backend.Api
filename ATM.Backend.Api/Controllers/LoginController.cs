05f16fc73ce8d7784d8ebc719d3fb16396310127
unknown <diogo2003.pimenta@gmail.com>
2025-12-22 22:03:52 +0000

    Initial commit - ASP.NET Core Web API

---
+++ ATM.Backend.Api/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ATM.Backend.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        // Endpoint de teste
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("API ATM a funcionar");
+        }
+    }
+}
