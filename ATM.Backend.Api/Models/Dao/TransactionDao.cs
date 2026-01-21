f47f076bf75395a3ebae2aa8af439da7e4ace162
0lavo <olavo.goncalves2005@gmail.com>
2026-01-21 22:45:50 +0000

    List transactions by account id

--- ATM.Backend.Api/Models/Dao/TransactionDao.cs
+++ ATM.Backend.Api/Models/Dao/TransactionDao.cs
@@ -1,5 +1,6 @@
 ﻿using ATM.Backend.Api.Models;
 using ATM.Backend.Api.Models.DbConnection;
+using Microsoft.EntityFrameworkCore;
 
 namespace ATM.Backend.Api.Repositories;
 
@@ -7,5 +8,24 @@ public class TransactionDao : GenericDao<Transaction>
 {
     public TransactionDao(AppDbContext context) : base(context)
     {
+
+        
+    }
+    
+    public List<Transaction> ListTransactionByAccountId(int accountId)
+    {
+        var accountTransactions = _context.Transactions
+            // (Optional) Include related data if you need to display it
+            .Include(t => t.SorceCard)
+            .Include(t => t.DestinyCard)
+            // The Core Logic
+            .Where(t => 
+                (t.SorceCard.Account.Id == accountId) || 
+                (t.DestinyCard.Account.Id == accountId)
+            )
+            .OrderByDescending(t => t.Date) // Usually you want the newest first
+            .ToList();
+
+        return accountTransactions;
     }
 }
